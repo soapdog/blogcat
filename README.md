b122a8559296233f036ba58e5d4a6d1b512dc0f8
Andre Alves Garzia <andre@andregarzia.com>
2020-06-04 21:41:33 +0100

    better styles

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Blogcat uses some third-party resources which are included in the `vendor/` fold
 * [Dexie](http://dexie.org) version 2.0.4
 * [Mithril](http://mithril.js.org) version 2.0.4 (a coincidence with the Dexie version, what are the odds...)
 * [Fontawesome](http://fontawesome.com) version 5.13.0 
+
+# Icons
+
+Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- subscribe/subscribe.js
+++ subscribe/subscribe.js
@@ -5,6 +5,7 @@ const subscribeView = () => {
     let atom = false
     let selectedFeed = false
     let title = ""
+    let stage = "findfeeds" // findfeeds, checksubscribed, showchoice
 
     browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         const tab = tabs[0]
@@ -25,6 +26,12 @@ const subscribeView = () => {
                     atom = atomUrl.href
                 }
 
+                if (rss || atom) {
+                    stage = "showchoice"
+                } else {
+                    stage = "nofeeds"
+                }
+
                 m.redraw()
 
             })
@@ -32,13 +39,15 @@ const subscribeView = () => {
 
     });
 
-    const logme = ev => {
+    const feedChange = ev => {
         let url = ev.target.value
+        console.log("attempting to fetch", url)
+        selectedFeed = false
         blogcat.fetchFeed(url)
             .then(feed => {
                 console.log("feed", feed)
                 title = feed.title
-                selectedFeed = feed.feedUrl
+                selectedFeed = feed.feedUrl || url
                 m.redraw()
             })
             .catch(err => console.error(err))
@@ -51,33 +60,52 @@ const subscribeView = () => {
             })
             .catch(error => console.error("err", error))
     }
+
+    const preview = () => {
+        browser.tabs.create({
+            url: `/preview/preview.html?query=${selectedFeed}`
+        });
+        window.close();
+    }
+
     return {
         view: vnode => {
-            if (rss || atom) {
-                return m(".container", [
-                    m(".columns", [
-                        m(".column", [
-                            m("form.m-2", [
-                                m(".form-group", [
-                                    m("label.form-label[for=title-input]", "Title"),
-                                    m("input.form-input[type=text][id=title-input]", { value: title })
-                                ]),
-                                m(".form-group", { onchange: logme }, [
-                                    m("label.form-label", "Feed"),
-                                    rss ? m("label.form-radio", [m("input[type=radio]", { name: "feed", value: rss }), m("i.form-icon"), m("span", rss)]) : "",
-                                    atom ? m("label.form-radio", [m("input[type=radio]", { name: "feed", value: atom }), m("i.form-icon"), m("span", atom)]) : ""
-                                ]),
-                                m("button.btn.btn-primary", { disabled: !selectedFeed, onclick: subscribe }, "Subscribe")
+            switch (stage) {
+                case "showchoice":
+                    return m(".container", [
+                        m(".columns", [
+                            m(".column", [
+                                m("form.m-2", [
+                                    m(".form-group", [
+                                        m("label.form-label[for=title-input]", "Title"),
+                                        m("input.form-input[type=text][id=title-input]", { value: title })
+                                    ]),
+                                    m(".form-group", { onchange: feedChange }, [
+                                        m("label.form-label", "Feed"),
+                                        rss ? m("label.form-radio", [m("input[type=radio]", { name: "feed", value: rss }), m("i.form-icon"), m("span", rss)]) : "",
+                                        atom ? m("label.form-radio", [m("input[type=radio]", { name: "feed", value: atom }), m("i.form-icon"), m("span", atom)]) : ""
+                                    ]),
+                                    m("button.btn.btn-primary", { disabled: !selectedFeed, onclick: subscribe }, "Subscribe"),
+                                    m("button.btn.ml-2", { disabled: !selectedFeed, onclick: preview }, "Preview")
+                                ])
                             ])
                         ])
                     ])
-                ])
-            } else {
-                return m(".container", [
-                    m(".empty", [
-                        m("p.empty-title.h5", "No feed found on page.")
+                case "nofeeds":
+                    return m(".empty", [
+                        m(".empty-icon", [
+                            m("img.p-centered", { src: "/assets/icons/cat_color.svg", style: "width:36px" })
+                        ]),
+                        m("p.empty-title.h5", "No feeds found on the page.")
                     ])
-                ])
+                case "findfeeds":
+                    return m(".empty", [
+                        m(".empty-icon", [
+                            m(".loading.loading-lg")
+                        ]),
+                        m("p.empty-title.h5.text-center", "Looking for feeds.")
+                    ])
+
             }
         }
     }
